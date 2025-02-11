6f056966784222992f3ffd1c80970cdd7d304fd0
Nick Waddoups <nick.waddoups@usu.edu>
2025-02-11 09:43:13 -0700

    Added empty definitions for operator()

--- include/solvers/static_tree.hpp
+++ include/solvers/static_tree.hpp
@@ -1,4 +1,4 @@
-#pragma once 
+#pragma once
 
 #include "concepts.hpp"
 #include "types.hpp"
@@ -12,10 +12,22 @@ class StaticTree
     static constexpr std::string_view name = "StaticTree";
     /// `StaticTree.operator()` ...
     [[nodiscard]] auto operator()([[maybe_unused]] const Config &_, const std::span<fp> dataset) const -> Bin;
+
+    struct impl
+    {
+        using BinRanges = std::array<fp, config.bins>;
+    };
 };
 
 // The `StaticTree` class must satisfy the `Solver` concept
 constexpr Config config = {};
 static_assert(Solver<StaticTree<config>>);
+
+template<Config config>
+[[nodiscard]]
+auto StaticTree<config>::operator()([[maybe_unused]] const Config &_, const std::span<fp> dataset) const -> Bin
+{
+    return {};
+}
 
 } // namespace hpc
